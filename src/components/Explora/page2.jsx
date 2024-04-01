68cd1496dbdc316d056e2a4b2a8fd1d2e6bad4ac
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2024-04-01 08:56:12 -0500

    links

--- src/components/Explora/page2.jsx
+++ src/components/Explora/page2.jsx
@@ -17,41 +17,44 @@ const page2 = () => {
 
       <div className="containCards">
         <div className="card">
-          
+
+          <a href="https://my.archdaily.co/co/@coravi/folders/referencias-urbanismo" target="_blank" rel="noopener noreferrer">
             <div className="card-front">
             <img className='imagen3' src={image2} alt="" />
             <h3 className="textimgina monserat">Urbanismo</h3>
             </div>
-            
+          </a>
           
         </div>
 
         <div className="card">
-          
+        <a href="https://my.archdaily.co/co/@coravi/folders/referencias-vivienda" target="_blank" rel="noopener noreferrer">
             <div className="card-front">
             <img className='imagen3' src={image3} alt="" />
             <h3 className="textimgina monserat">Vivienda</h3>
             </div>
-        
+          </a>
           
         </div>
 
         <div className="card">
-          
+        <a href="https://my.archdaily.co/co/@coravi/folders/referencias-comercio" target="_blank" rel="noopener noreferrer">
             <div className="card-front">
             <img className='imagen3' src={image4} alt="" />
             <h3 className="textimgina monserat">Comercio</h3>
             </div>
-            
+            </a>
           
         </div>
 
         <div className="card">  
+        <a href="https://my.archdaily.co/co/@coravi/folders/referencias-interiores" target="_blank" rel="noopener noreferrer">
             <div className="card-front">
             <img className='imagen3' src={image5} alt="" />
             <h3 className="textimgina monserat">Interiores</h3>
             </div>
-        </div>
+           </a>
+        </div> 
       </div>
       <div className="espesiood">
 
